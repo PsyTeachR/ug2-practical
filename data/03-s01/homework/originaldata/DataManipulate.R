fad2c58f75725550177563319cff5eabab8294f5
philmcaleer <33661378+philmcaleer@users.noreply.github.com>
2020-05-11 15:23:03 +0100

    Delete DataManipulate.R

--- data/03-s01/homework/originaldata/DataManipulate.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-dat2016 <- read_csv("Happiness2016.csv") %>% select(Country, Region)
-dat2017 <- read_csv("Happiness2017.csv") %>% 
-  left_join(dat2016, ., by = "Country") %>%
-  arrange(Happiness.Rank) %>%
-  filter(!is.na(Happiness.Rank))
-write.csv(dat2017, "Happiness2017a.csv", row.names = FALSE)
